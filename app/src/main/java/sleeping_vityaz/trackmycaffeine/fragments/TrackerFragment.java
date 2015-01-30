b241df57a296871dedd025dfde30201fe90d0d42
sp574 <sp574@cornell.edu>
2015-01-30 09:13:23 -0500

    Implemented formula for caffeine concentration in blood

--- app/src/main/java/sleeping_vityaz/trackmycaffeine/fragments/TrackerFragment.java
+++ app/src/main/java/sleeping_vityaz/trackmycaffeine/fragments/TrackerFragment.java
@@ -3,12 +3,14 @@ package sleeping_vityaz.trackmycaffeine.fragments;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import sleeping_vityaz.trackmycaffeine.R;
@@ -22,6 +24,7 @@ public class TrackerFragment extends Fragment {
     private String[] colors;
     private Handler handler;
     private Runnable r;
+    private long startTime = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,13 +56,16 @@ public class TrackerFragment extends Fragment {
                /* else if (currV == -60 && !go)
                     go = true;*/
 
-                if (go)
-                    currV++;/*
-                else
-                    currV--;*/
+                long millis = System.currentTimeMillis() - startTime;
+                int seconds = (int) (millis / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
 
-                meter.setValues(currV, currV, currV);
-                handler.postDelayed(this, 100);
+                if (go)
+                    currV++;
+
+                meter.setValues(seconds, seconds, 0);
+                handler.postDelayed(this, 500);
             }
         };
 

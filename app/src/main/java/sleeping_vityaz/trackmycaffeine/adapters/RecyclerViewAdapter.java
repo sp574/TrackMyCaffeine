0800bb5ea8c361bfc08e725eaa77d9562418d471
sp574 <sp574@cornell.edu>
2015-01-31 22:23:56 -0500

    Lots of work done in an hour! - Implemented RecyclerView for the tracker fragment - Connected RecyclerView with the database: so now can add/delete to/from the DB! TODO: add drinks-db to the app and get information from it

---
+++ app/src/main/java/sleeping_vityaz/trackmycaffeine/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,121 @@
+package sleeping_vityaz.trackmycaffeine.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.swipe.SimpleSwipeListener;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+import com.daimajia.swipe.implments.SwipeItemRecyclerMangerImpl;
+
+import sleeping_vityaz.trackmycaffeine.DBTools;
+import sleeping_vityaz.trackmycaffeine.R;
+import sleeping_vityaz.trackmycaffeine.util.CommonConstants;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapter.SimpleViewHolder> {
+
+    DBTools dbTools = null;
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        SwipeLayout swipeLayout;
+        TextView tv_keyId, tv_caffeine_mass, tv_product, tv_date, tv_time, tv_drink_volume;
+        Button buttonDelete;
+
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            swipeLayout = (SwipeLayout) itemView.findViewById(R.id.swipe);
+            tv_keyId = (TextView) itemView.findViewById(R.id.key_id);
+            tv_caffeine_mass = (TextView) itemView.findViewById(R.id.tv_caffeine_mass);
+            tv_product = (TextView) itemView.findViewById(R.id.tv_product);
+            tv_date = (TextView) itemView.findViewById(R.id.tv_date);
+            tv_time = (TextView) itemView.findViewById(R.id.tv_time);
+            tv_drink_volume = (TextView) itemView.findViewById(R.id.tv_drink_volume);
+            buttonDelete = (Button) itemView.findViewById(R.id.delete);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(getClass().getSimpleName(), "onItemSelected: " + tv_product.getText().toString());
+                    Toast.makeText(view.getContext(), "onItemSelected: " + tv_product.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private Context mContext;
+    private ArrayList<HashMap<String, String>> mDataset;
+
+    //protected SwipeItemRecyclerMangerImpl mItemManger = new SwipeItemRecyclerMangerImpl(this);
+
+    public RecyclerViewAdapter(Context context, ArrayList<HashMap<String, String>> objects) {
+        this.mContext = context;
+        this.mDataset = objects;
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final SimpleViewHolder viewHolder, final int position) {
+        HashMap<String, String> recordMap = mDataset.get(position);
+        dbTools = DBTools.getInstance(mContext);
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+        viewHolder.swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
+            }
+        });
+        viewHolder.swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
+            @Override
+            public void onDoubleClick(SwipeLayout layout, boolean surface) {
+                Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+                mDataset.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, mDataset.size());
+                mItemManger.closeAllItems();
+                dbTools.deleteRecord(viewHolder.tv_keyId.getText().toString());
+                Toast.makeText(view.getContext(), "Deleted " + viewHolder.tv_product.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.tv_keyId.setText(recordMap.get(CommonConstants.KEY_ID));
+        viewHolder.tv_caffeine_mass.setText(recordMap.get(CommonConstants.CAFFEINE_MASS));
+        viewHolder.tv_product.setText(recordMap.get(CommonConstants.PRODUCT));
+        viewHolder.tv_date.setText(recordMap.get(CommonConstants.DATE_CREATED));
+        viewHolder.tv_time.setText(recordMap.get(CommonConstants.TIME_STARTED));
+        viewHolder.tv_drink_volume.setText(recordMap.get(CommonConstants.DRINK_VOLUME));
+        mItemManger.bindView(viewHolder.itemView, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+    }
+}
--- app/src/main/java/sleeping_vityaz/trackmycaffeine/fragments/TrackerFragment.java
+++ app/src/main/java/sleeping_vityaz/trackmycaffeine/fragments/TrackerFragment.java
@@ -1,29 +1,30 @@
 package sleeping_vityaz.trackmycaffeine.fragments;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Chronometer;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import com.daimajia.swipe.SwipeLayout;
-import com.daimajia.swipe.implments.SwipeItemMangerImpl;
+
+import jp.wasabeef.recyclerview.animators.FadeInLeftAnimator;
+import com.daimajia.swipe.util.Attributes;
 import com.github.lzyzsd.circleprogress.ArcProgress;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import sleeping_vityaz.trackmycaffeine.DBTools;
 import sleeping_vityaz.trackmycaffeine.R;
-import sleeping_vityaz.trackmycaffeine.adapters.ListViewAdapter;
-import sleeping_vityaz.trackmycaffeine.circularcounter.CircularCounter;
+import sleeping_vityaz.trackmycaffeine.adapters.RecyclerViewAdapter;
+import sleeping_vityaz.trackmycaffeine.adapters.util.DividerItemDecoration;
 import sleeping_vityaz.trackmycaffeine.util.Calculations;
 
 
@@ -34,52 +35,24 @@ public class TrackerFragment extends Fragment {
 
     private TextView date, start, duration, results;
 
-   // private CircularCounter meter;
-    /*private CircularCounter meter;
-    private String[] colors;
-    private Handler handler;
-    private Runnable r;
-    private long startTime = 0;*/
-
-
-    private ListView mListView;
-    private ListViewAdapter mAdapter;
     private Context mContext = this.getActivity();
     private ArcProgress arcProgress;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<HashMap<String, String>> mDataSet;
+
+    DBTools dbTools = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_tracker, container, false);
 
-        mListView = (ListView) rootView.findViewById(R.id.listview);
-        arcProgress = (ArcProgress) rootView.findViewById(R.id.arc_progress);
+        dbTools = DBTools.getInstance(this.getActivity());
 
-        mAdapter = new ListViewAdapter(getActivity());
-        mListView.setAdapter(mAdapter);
-        mAdapter.setMode(SwipeItemMangerImpl.Mode.Single);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ((SwipeLayout)(mListView.getChildAt(position - mListView.getFirstVisiblePosition()))).open(true);
-            }
-        });
+        findViewsById(rootView);
 
-        mListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.e("ListView", "onItemSelected:" + position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.e("ListView", "onNothingSelected:");
-            }
-        });
-
-        date = (TextView) rootView.findViewById(R.id.tv_date);
-        start = (TextView) rootView.findViewById(R.id.tv_start);
-        duration = (TextView) rootView.findViewById(R.id.tv_duration);
-        results = (TextView) rootView.findViewById(R.id.tv_results);
+        recyclyViewSetUp(rootView);
 
         final int i_date = 21*3600*1000, i_start = 20*3600*1000;
 
@@ -87,40 +60,53 @@ public class TrackerFragment extends Fragment {
         results.setText("Concentration of caffeine: " + concentration);
         alert("" + concentration);
         arcProgress.setProgress((int) concentration);
-       /* colors = getResources().getStringArray(R.array.colors);
-        meter = (CircularCounter) rootView.findViewById(R.id.meter);
-        meter.setFirstWidth(getResources().getDimension(R.dimen.first))
-                .setFirstColor(getResources().getColor(R.color.primary_dark))
-                .setSecondWidth(getResources().getDimension(R.dimen.second))
-                .setSecondColor(getResources().getColor(R.color.accent))
-                .setBackgroundColor(getResources().getColor(R.color.white));
-
-        handler = new Handler();
-        r = new Runnable() {
-            int currV = 0;
-            boolean go = true;
-
-            public void run() {
-                if (currV == 60 && go)
-                    go = false;
-               *//* else if (currV == -60 && !go)
-                    go = true;*//*
-
-                long millis = System.currentTimeMillis() - startTime;
-                int seconds = (int) (millis / 1000);
-                int minutes = seconds / 60;
-                seconds = seconds % 60;
-
-                if (go)
-                    currV++;
-
-                meter.setValues(seconds, 0, 0);
-                handler.postDelayed(this, 500);
-            }
-        };*/
-
 
         return rootView;
+    }
+
+    private void recyclyViewSetUp(View rootView) {
+        // Layout Managers:
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        // Item Decorator:
+        recyclerView.addItemDecoration(new DividerItemDecoration(getResources().getDrawable(R.drawable.divider)));
+        recyclerView.setItemAnimator(new FadeInLeftAnimator());
+
+        // Adapter:
+        mDataSet = dbTools.getAllRecords();
+        mAdapter = new RecyclerViewAdapter(this.getActivity(), mDataSet);
+        ((RecyclerViewAdapter) mAdapter).setMode(Attributes.Mode.Single);
+        recyclerView.setAdapter(mAdapter);
+
+        /* Listeners */
+        recyclerView.setOnScrollListener(onScrollListener);
+    }
+
+    /**
+     * Substitute for our onScrollListener for RecyclerView
+     */
+    RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+            //Log.e("ListView", "onScrollStateChanged");
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            // Could hide open views here if you wanted. //
+        }
+    };
+
+    private void findViewsById(View rootView) {
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        arcProgress = (ArcProgress) rootView.findViewById(R.id.arc_progress);
+        date = (TextView) rootView.findViewById(R.id.tv_date);
+        start = (TextView) rootView.findViewById(R.id.tv_start);
+        duration = (TextView) rootView.findViewById(R.id.tv_duration);
+        results = (TextView) rootView.findViewById(R.id.tv_results);
+
     }
 
     @Override
